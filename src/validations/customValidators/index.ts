4ee69c6a1ff11009754b7696927b3c230115ed6c
Taron Vardanyan <taron.vardanyan@betconstruct.com>
2022-09-17 16:15:10 +0400

    added sign in validation

--- src/validations/customValidators/index.ts
+++ src/validations/customValidators/index.ts
@@ -1,5 +1,13 @@
 import { CustomValidator } from "joi";
 import isString from "lodash.isstring";
+import zxcvbn from "zxcvbn";
+
+export const joiGuessablePassword: CustomValidator = (value, helper) => {
+  if (zxcvbn(value).score < 2) {
+    return helper.error("any.invalid");
+  }
+  return value;
+};
 
 export const passwordValidation: CustomValidator = (value, helper) => {
   if (!isString(value) || /^((?! +).)*$/.test(value.trim()) === false) {
--- src/validations/elements/password/validation.ts
+++ src/validations/elements/password/validation.ts
@@ -1,7 +1,11 @@
 import Joi from "joi";
-import { passwordValidation } from "../../customValidators";
+import {
+  passwordValidation,
+  joiGuessablePassword,
+} from "../../customValidators";
 
 export const passwordSchema = Joi.string()
   .min(8)
   .max(32)
-  .custom(passwordValidation, "custom password validation");
+  .custom(passwordValidation, "custom password validation")
+  .custom(joiGuessablePassword);
