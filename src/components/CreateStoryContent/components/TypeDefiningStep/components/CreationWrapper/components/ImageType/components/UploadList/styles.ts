b22e6506c2a690bbc0ead76ab96f265e090df36e
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-02-20 19:16:22 +0400

    added upload area

---
+++ src/components/CreateStoryContent/components/TypeDefiningStep/components/CreationWrapper/components/ImageType/components/UploadList/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const UploadListWrapper = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
