d3ab7e81aba167771bdee9c3efd5dc67cd34b269
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-12 11:50:20 +0400

    add Fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react";
 import AnimatedRoutes from "./AnimatedRoutes";
+import Fallback from "@components/Fallback";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Suspense fallback={<h1>Loading...</h1>}>
+      <Suspense fallback={<Fallback />}>
         <AnimatedRoutes />
       </Suspense>
     </Router>
---
+++ src/components/Fallback/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const FallbackWrapper = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
