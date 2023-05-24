165203fc83dee1c7b78a39559e227c9c54dd4b29
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-05-24 16:15:29 +0400

    add step 3 responsivness

---
+++ src/shared/PreviewMobileDrawer/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from "react";
+import { Drawer } from "antd";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+  title?: string;
+  isOpen: boolean;
+}
+
+const PreviewMobileDrawer = ({ children, onClose, title, isOpen }: Props) => {
+  return (
+    <Drawer
+      getContainer={false}
+      mask={false}
+      contentWrapperStyle={{
+        transform: "-90deg",
+      }}
+      title={title}
+      onClose={onClose}
+      width="100%"
+      open={isOpen}
+    >
+      {children}
+    </Drawer>
+  );
+};
+
+export default PreviewMobileDrawer;
