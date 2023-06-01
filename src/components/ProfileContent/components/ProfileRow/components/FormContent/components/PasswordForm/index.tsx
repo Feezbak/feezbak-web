1f3dc4fce17b0d623dd27b275a938b8d18c7adf1
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-06-01 20:37:19 +0400

    add fixes for animation in Profile section

---
+++ src/components/ProfileContent/components/ProfileRow/components/FormContent/components/PasswordForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { opacityAnimation } from "@assets/framerAnimations";
+import { Form } from "./styles";
+
+const PasswordForm = () => {
+  return <Form {...opacityAnimation}>Password</Form>;
+};
+
+export default PasswordForm;
--- src/components/ProfileContent/components/ProfileRow/index.tsx
+++ src/components/ProfileContent/components/ProfileRow/index.tsx
@@ -1,16 +1,15 @@
 import React, { lazy, useState } from "react";
+import FormContent from "./components/FormContent";
 import { ProfileRowWrapper } from "./styles";
 
 const Selector = lazy(() => import("./components/ProfileFormSelector"));
-const ProfileForm = lazy(() => import("./components/ProfileForm"));
-//const PasswordForm = lazy(() => import("./components/PasswordForm"));
 
 const ProfileRow = () => {
   const [activeForm, setActiveForm] = useState(0);
   return (
     <ProfileRowWrapper>
       <Selector active={activeForm} setActive={setActiveForm} />
-      <ProfileForm />
+      <FormContent activeForm={activeForm} />
     </ProfileRowWrapper>
   );
 };
