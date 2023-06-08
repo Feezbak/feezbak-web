82d064a74c9b1f1481d76d2c6f919f84fd2ca215
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-06-08 22:13:28 +0400

    fix(root): add sync work of commitizen and husky

--- src/components/FeedbackerContent/index.tsx
+++ src/components/FeedbackerContent/index.tsx
@@ -20,7 +20,6 @@ const FeedbackerContent = () => {
     <FeedbackerContentWrapper>
       <Header />
       <PreviewFlowWrapper>Preview Wrapper</PreviewFlowWrapper>
-      <div>GGGG</div>
     </FeedbackerContentWrapper>
   );
 };
