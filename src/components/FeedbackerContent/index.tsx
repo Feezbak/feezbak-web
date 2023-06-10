a99df05924c71b39bc0671f05f666cdfc8f3e79c
TaronVardanyan <taron.vardanyan@softconstruct.com>
2023-06-10 20:42:05 +0400

    test(root): need to test a new docker image creation and update process

--- src/components/FeedbackerContent/index.tsx
+++ src/components/FeedbackerContent/index.tsx
@@ -19,7 +19,7 @@ const FeedbackerContent = () => {
   ) : (
     <FeedbackerContentWrapper>
       <Header />
-      <PreviewFlowWrapper>Preview Wrapper</PreviewFlowWrapper>
+      <PreviewFlowWrapper>Preview Wrapper Test</PreviewFlowWrapper>
     </FeedbackerContentWrapper>
   );
 };
