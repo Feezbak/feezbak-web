1a126894f95416841ddd647bc25c11c54f8b70fd
tar_vardanyan <taron@bl.ink>
2023-08-03 11:41:32 +0400

    add

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,8 +1,15 @@
 import "react-draft-wysiwyg"; // Import to trigger declaration merging
+import "antd";
 
 declare module "react-draft-wysiwyg" {
   // Augment the existing ButtonProps interface
   interface EditorProps {
     handleBeforeInput?: any;
+  }
+}
+
+declare module "antd" {
+  interface FormItemProps {
+    name?: string;
   }
 }
