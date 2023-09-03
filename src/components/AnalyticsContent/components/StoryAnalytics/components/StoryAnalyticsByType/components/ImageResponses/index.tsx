97df44d6f9817534bb185b262c5161a43891847a
tar_vardanyan <taron@bl.ink>
2023-09-03 22:43:56 +0400

    style(image-feedbacks): add UI for image type feedbacks

--- src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/index.tsx
+++ src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/index.tsx
@@ -1,9 +1,12 @@
+import { StoryTypeEnum } from "@/enums";
+
 interface Props {
   feedbacks: unknown;
+  storyType: StoryTypeEnum;
 }
 
-const ImageResponses = ({ feedbacks }: Props) => {
-  return <div>Image Response</div>;
+const ImageResponses = ({ feedbacks, storyType }: Props) => {
+  return <div>Image Response, create dynamic page rely on type</div>;
 };
 
 export default ImageResponses;
