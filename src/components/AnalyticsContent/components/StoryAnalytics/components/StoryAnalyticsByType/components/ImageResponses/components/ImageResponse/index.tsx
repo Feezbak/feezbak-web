59c8f6d81a5cfff450f2b7e86f00cbe8c6482e6e
tar_vardanyan <taron@bl.ink>
2023-09-05 23:17:54 +0400

    feat(anaylitcs): add image col for results

--- src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/components/ImageResponse/index.tsx
+++ src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/components/ImageResponse/index.tsx
@@ -1,6 +1,6 @@
-import { ImageResponseContainer } from "./styles";
 import { StoryTypeEnum } from "@/enums";
 import { ImageResponsesType } from "@/constants";
+import { ImageResponseContainer, ImageCol, Image } from "./styles";
 
 interface Props {
   data: ImageResponsesType;
@@ -8,7 +8,16 @@ interface Props {
 }
 
 const ImageResponse = ({ data, storyType }: Props) => {
-  return <ImageResponseContainer>fff</ImageResponseContainer>;
+  return (
+    <ImageResponseContainer>
+      <ImageCol xs={24} sm={24} md={5} lg={5} xl={5} xxl={5}>
+        <Image
+          src={`${process.env.REACT_APP_API_URL}/${data.src}`}
+          loading="lazy"
+        />
+      </ImageCol>
+    </ImageResponseContainer>
+  );
 };
 
 export default ImageResponse;
--- src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/components/ImageResponse/styles.ts
+++ src/components/AnalyticsContent/components/StoryAnalytics/components/StoryAnalyticsByType/components/ImageResponses/components/ImageResponse/styles.ts
@@ -1,6 +1,24 @@
 import styled from "styled-components";
-import { Row } from "antd";
+import { Row, Col } from "antd";
+import { StyleEnums } from "@/enums";
 
 export const ImageResponseContainer = styled(Row)`
   width: 100%;
+  padding: 2rem 0;
+  border-bottom: 1px solid ${StyleEnums.gray4};
+
+  &:last-child {
+    border: none;
+  }
+`;
+
+export const ImageCol = styled(Col)`
+  max-height: 15rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 0.75rem;
+  object-fit: cover;
 `;
