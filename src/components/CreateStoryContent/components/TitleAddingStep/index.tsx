d1c21e1d6fe7b278fa275af266d183655511fe06
tar_vardanyan <taron@bl.ink>
2023-10-24 21:53:26 +0400

    add bug fixes for layer fix

--- src/components/CreateStoryContent/components/TitleAddingStep/index.tsx
+++ src/components/CreateStoryContent/components/TitleAddingStep/index.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import CreationWrapper from "./components/CreationWrapper";
 import { Preview, PreviewMobileDrawer } from "@/shared";
 import { useResponsive } from "@/hooks";
-import { AnimatedWrapper } from "../../styles";
-import { opacityAnimation } from "@assets/framerAnimations";
+import { ContentWrapper } from "../../styles";
 
 const TitleAddingStep = () => {
   const [isDemoDrawerOpen, setDemoDrawerState] = useState(false);
@@ -14,7 +13,7 @@ const TitleAddingStep = () => {
   };
 
   return (
-    <AnimatedWrapper {...opacityAnimation}>
+    <ContentWrapper>
       <CreationWrapper handleDemo={handleDemo} />
       {isMobile ? (
         <PreviewMobileDrawer
@@ -27,7 +26,7 @@ const TitleAddingStep = () => {
       ) : (
         <Preview isCreationMode={true} />
       )}
-    </AnimatedWrapper>
+    </ContentWrapper>
   );
 };
 
