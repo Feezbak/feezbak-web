cabcc879f6350b24a2d6535b6ad278e1c451e22f
tar_vardanyan <taron@bl.ink>
2023-10-24 22:57:00 +0400

    fix(ui): add ui updates

--- src/assets/framerAnimations/index.ts
+++ src/assets/framerAnimations/index.ts
@@ -20,7 +20,8 @@ export const listItemAnimation = (
 export const opacityAnimation = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  transition: { duration: 0.3 },
+  exit: { opacity: 1 },
+  transition: { duration: 0.4 },
 };
 
 export const opacityWithScaleAnimation = {
