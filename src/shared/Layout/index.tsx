2557fc14e53cd227a34f36627d1c39b798e5b1be
tar_vardanyan <taron@bl.ink>
2023-10-31 23:03:56 +0400

    feat(layout): add fixes

--- src/shared/Layout/index.tsx
+++ src/shared/Layout/index.tsx
@@ -13,6 +13,35 @@ interface Props {
 const Layout = ({ pageTitle, children, isAnimated = true }: Props) => {
   useEffect(() => {
     document.title = pageTitle;
+
+    const sleek = (window as any)?.$sleek;
+    if (sleek && typeof sleek.hideButton === "function") {
+      if (pageTitle === PageTitleEnums.DASHBOARD) {
+        sleek.showButton();
+      } else {
+        sleek.hideButton();
+      }
+    } else {
+      if (pageTitle === PageTitleEnums.DASHBOARD) {
+        const script = document.createElement("script");
+
+        // Set attributes for the script tag
+        script.type = "text/javascript";
+
+        // Add your JavaScript code inside the script tag
+        script.innerHTML = `
+     window.$sleek=[];window.SLEEK_PRODUCT_ID=${process.env.SLEEK_PRODUCT_ID};(function(){d=document;s=d.createElement("script");s.src="https://client.sleekplan.com/sdk/e.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+    `;
+
+        // Append the script tag to the body
+        document.body.appendChild(script);
+
+        return () => {
+          // Clean up the script tag when the component unmounts
+          document.body.removeChild(script);
+        };
+      }
+    }
   }, [pageTitle]);
 
   return (
