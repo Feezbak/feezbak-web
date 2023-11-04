3a5a4b0cbc40be21e0d95feb42190a712d06cd6e
tar_vardanyan <taron@bl.ink>
2023-11-04 17:00:40 +0400

    add UI updates

--- src/components/AnalyticsContent/index.tsx
+++ src/components/AnalyticsContent/index.tsx
@@ -7,7 +7,7 @@ const AnalyticsContent = () => {
   const { storyId } = useParams();
 
   return (
-    <AnalyticsWrapper xs={20} sm={20} md={20} lg={20} xl={15} xxl={15}>
+    <AnalyticsWrapper xs={22} sm={22} md={20} lg={20} xl={15} xxl={15}>
       <NavigationHeader
         links={links(storyId!)}
         mobileMenuLinks={getMobileMenuLinks(storyId!)}
--- src/components/DashboardContent/index.tsx
+++ src/components/DashboardContent/index.tsx
@@ -7,7 +7,7 @@ import { DashboardContentWrapper } from "./styles";
 
 const DashboardContent = () => {
   return (
-    <DashboardContentWrapper xs={20} sm={20} md={20} lg={20} xl={15} xxl={15}>
+    <DashboardContentWrapper xs={22} sm={22} md={20} lg={20} xl={15} xxl={15}>
       <NavigationHeader links={links} mobileMenuLinks={mobileMenuLinks} />
       <Outlet />
     </DashboardContentWrapper>
