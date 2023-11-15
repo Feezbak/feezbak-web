11341b31d4795276f87e276aa157dd7111e8b1a5
tar_vardanyan <taron@bl.ink>
2023-11-16 01:23:19 +0400

    style(client-layers): add ui updates

--- src/shared/Preview/components/ClientLayers/components/WelcomeLayer/index.tsx
+++ src/shared/Preview/components/ClientLayers/components/WelcomeLayer/index.tsx
@@ -19,25 +19,14 @@ const WelcomeLayer = ({ handleLayer, handleSkip }: Props) => {
         <h2>Welcome to Feezbak 👋🏻</h2>
         <p>
           We're thrilled to have you here and we greatly value your feedback.
-          <br />
-          <br />
-          Thank you for taking the time to share your thoughts with us. Your
-          feedback matters, and we're excited to have you on board as we work
-          together to make our web app even better. Enjoy your time exploring
-          and using our platform, and feel free to reach out to us if you have
-          any questions or need assistance along the way.
-          <br />
-          <br />
-          Welcome once again, and let's embark on this feedback journey
-          together!
         </p>
       </LayerInfoContent>
       <ActionsWrapper>
         <NextBtn onClick={handleLayer} size="large">
-          Next
+          Learn About Feezbak
         </NextBtn>
         <SkipBtn onClick={handleSkip} size="large">
-          Skip {">>>"}
+          Give Feedback
         </SkipBtn>
       </ActionsWrapper>
     </LayerWrapper>
